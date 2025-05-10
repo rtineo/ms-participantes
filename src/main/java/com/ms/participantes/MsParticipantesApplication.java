a684cdcda359c5168eae2cf1c3dbdb3ee2bcda7e
Ronald <rtineo@sunat.gob.pe>
2025-05-09 22:51:58 -0500

    se sube proyecto inicial

---
+++ src/main/java/com/ms/participantes/MsParticipantesApplication.java
@@ -0,0 +1,13 @@
+package com.ms.participantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsParticipantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsParticipantesApplication.class, args);
+	}
+
+}
